2e5c5a7421ab8e0b0eaa37bc8573faecc9fd13a2
Yoman <603820467@qq.com>
2020-04-24 18:00:28 +0800

    完善LayoutManagerFinal2，但是左右滑动到底还是会调用mCreatedView

--- app/src/main/java/com/example/testproject/recyclerview/LayoutManagerFinal2.java
+++ app/src/main/java/com/example/testproject/recyclerview/LayoutManagerFinal2.java
@@ -1,9 +1,13 @@
 package com.example.testproject.recyclerview;
 
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,8 +23,21 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
     private int mPositionSelectedItem;
     //当前位移的总距离
     private int mTotalOffsetX;
+    private ValueAnimator mAnimator;
+    private RecyclerView.Recycler mRecycler;
     //当前屏幕的Rect
     private Rect mRectScreen = new Rect(0, 0, getRecyclerViewWidth(), getRecyclerViewHeight());
+
+    //当前开始的位移量
+    private int mStartOffsetX;
+
+    private final int DIRECTION_LEFT_TO_RIGHT = 1;
+    private final int DIRECTION_RIGHT_TO_LEFT = 2;
+
+    @Override
+    public boolean canScrollHorizontally() {
+        return true;
+    }
 
     //获取屏幕上能容纳的最大子View个数
     private int getScreenViewMaxCount() {
@@ -50,15 +67,22 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
      */
     @Override
     public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
-        Log.e("xiaojun", "onlayoutChildren");
         if (getItemCount() == 0) {
             detachAndScrapAttachedViews(recycler);
             return;
         }
+        //recyclerView结构未改变就不重新布局
+        if (!state.didStructureChange())
+            return;
+//        Log.e("xiaojun", "onLayoutChildren");
+        mRecycler = recycler;
+
+        //保存上一次所有items的数量
+        int itemsLastCount = mItems.size();
 
         //---------------------------------------------测量-----------------------------------------------
         //如果总Item数量和保存的Rect的数量相等，就不用测量，否则就测量
-        if (getItemCount() != mItems.size()) {
+        if (getItemCount() != itemsLastCount) {
             mItems.clear();//清除之前保存的所有item
             View child = recycler.getViewForPosition(0);
             addView(child);
@@ -66,16 +90,18 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
             int width = getDecoratedMeasuredWidth(child);
             int height = getDecoratedMeasuredHeight(child);
             mWidth = width;
+            mStartOffsetX = getRecyclerViewWidth() / 2 - mWidth / 2;
             detachAndScrapView(child, recycler);//测量完成之后就将这个View放回缓存器
             int offsetX = 0;
-            //保存所有item的位置
+            //保存所有item的位置(所有的子Item的位置都需要加上初始偏移量)
             for (int i = 0; i < getItemCount(); i++) {
-                Rect rect = new Rect(offsetX, 0, offsetX + width, height);
+                Rect rect = new Rect(offsetX + mStartOffsetX, 0, offsetX + width + mStartOffsetX, height);
                 mItems.put(i, rect);
                 offsetX += width;
             }
             mTotalItemWidth = offsetX;
         }
+
         //数据集变化(1.notifyDataSetChange)
         //1.如果最新的数据集的个数 < 之前的数据集个数 :
         //(1)需要判断 mPositionSelected 当前选中的View的Position是否大于 最新数据集 的最大index
@@ -84,7 +110,7 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
             int totalItemCount = getItemCount();
             //如果新的数据集的总数量 > 屏幕能容纳的最大子View数量
             if (totalItemCount >= getScreenViewMaxCount()) {
-                if (totalItemCount < mItems.size()) {
+                if (totalItemCount < itemsLastCount) {
                     //如果之前选中的位置已经超过了 最新 item 数量的最大index，
                     if (mPositionSelectedItem > totalItemCount - 1) {
                         mPositionSelectedItem = totalItemCount - 1;
@@ -102,30 +128,26 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
                 mPositionSelectedItem = 0;
             }
         }
-
         //离屏缓存
         detachAndScrapAttachedViews(recycler);
-
         layoutChildren(recycler);
 
     }
 
     //布局应该显示在屏幕上的View，根据当前位移的距离
     private void layoutChildren(RecyclerView.Recycler recycler) {
-        Rect rectRecyclerView = getScreenRectWithTotalOffsetX();
-        int visibleCount = getScreenViewMaxCount();
-        Log.e("xiaojun", "visibleCount=" + visibleCount);
-        for (int i = 0, visibleNum = 0; i < mItems.size() && visibleNum < visibleCount; i++) {
+        Rect rectRecyclerView = getRecyclerViewRect();
+        for (int i = 0; i < mItems.size(); i++) {
             Rect rect = mItems.get(i);
-            View child = recycler.getViewForPosition(i);
             //相交的child才显示出来
             if (Rect.intersects(rectRecyclerView, rect)) {
+                View child = recycler.getViewForPosition(i);
                 addView(child);
                 measureChild(child, 0, 0);
                 layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
-                visibleNum++;
             }
         }
+        getCurrentSelectedItemPosition();
     }
 
     /**
@@ -133,35 +155,64 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
      *
      * @return
      */
-    private Rect getScreenRectWithTotalOffsetX() {
+    private Rect getRecyclerViewRect() {
         mRectScreen.set(mTotalOffsetX, 0, mTotalOffsetX + getRecyclerViewWidth(), getRecyclerViewHeight());
         return mRectScreen;
     }
 
-    @Override
-    public boolean canScrollHorizontally() {
-        return true;
+    public void scrollToPositionCustom(int position) {
+        if (mAnimator != null && mAnimator.isRunning())
+            mAnimator.cancel();
+        Rect rectRecyclerView = getRecyclerViewRect();
+        int centerLineX = (rectRecyclerView.right-rectRecyclerView.left)/2+rectRecyclerView.left;
+        Rect rectSelected = mItems.get(position);
+        int centerSeleceted = (rectSelected.right-rectSelected.left)/2+rectSelected.left;
+
+        mAnimator = ValueAnimator.ofFloat(mTotalOffsetX, mItems.get(position).left-mStartOffsetX);
+        mAnimator.setDuration(500);
+        mAnimator.setInterpolator(new DecelerateInterpolator());
+
+        int direction = centerSeleceted<centerLineX?DIRECTION_LEFT_TO_RIGHT:DIRECTION_RIGHT_TO_LEFT;
+        mAnimator.addUpdateListener(animation -> {
+            float value = (float) animation.getAnimatedValue();
+            mTotalOffsetX = (int) value;
+            recyclerAndLayoutViews(mRecycler, direction);
+        });
+        mAnimator.start();
     }
 
-    private int DIRECTION_LEFT_TO_RIGHT = 1;
-    private int DIRECTION_RIGHT_TO_LEFT = 2;
 
     @Override
     public int scrollHorizontallyBy(int dx, RecyclerView.Recycler recycler, RecyclerView.State state) {
-        mTotalOffsetX += dx;
-        int direction = dx > 0 ? DIRECTION_RIGHT_TO_LEFT : DIRECTION_LEFT_TO_RIGHT;
+        //校正位移距离dx
+        int realDx = correctionDx(dx);
+        mTotalOffsetX += realDx;
+        int direction = realDx > 0 ? DIRECTION_RIGHT_TO_LEFT : DIRECTION_LEFT_TO_RIGHT;
+        recyclerAndLayoutViews(recycler, direction);
+        return realDx;
+    }
 
-        if (getChildCount() == 0)
-            return 0;
-        View childFirst = getChildAt(0);
-        View childLast = getChildAt(getChildCount() - 1);
-        int positionFirst = getPosition(childFirst);
-        int positionLast = getPosition(childLast);
-        recyclerViews(recycler, direction, positionFirst, positionLast);
-        addItem(recycler, direction, positionFirst, positionLast);
-//        offsetChildrenHorizontal(-dx);
-        Log.e("xiaojun", "当前位移距离:" + mTotalOffsetX);
-        return dx;
+    /**
+     * 矫正位移的距离
+     *
+     * @param dx
+     */
+    private int correctionDx(int dx) {
+        int realDx = dx;
+        Rect rectRecyclerView = getRecyclerViewRect();
+        //←
+        if (dx > 0) {
+            if (rectRecyclerView.left + dx > mTotalItemWidth + mStartOffsetX * 2 - getRecyclerViewWidth()) {
+                realDx = mTotalItemWidth - getRecyclerViewWidth() - rectRecyclerView.left + mStartOffsetX * 2;
+            }
+        }
+        //→
+        else if (dx < 0) {
+            if (rectRecyclerView.left + dx < 0) {
+                realDx = -rectRecyclerView.left;
+            }
+        }
+        return realDx;
     }
 
     /**
@@ -169,79 +220,126 @@ public class LayoutManagerFinal2 extends RecyclerView.LayoutManager {
      *
      * @param recycler
      */
-    private void recyclerViews(RecyclerView.Recycler recycler, int direction, int positionFirst, int positionLast) {
-        Rect rectRecyclerView = getScreenRectWithTotalOffsetX();
-        Log.e("xiaojun", "RecyclerView的rect.left=" + rectRecyclerView.left + ",mTotalOffsetX=" + mTotalOffsetX);
+    private void recyclerAndLayoutViews(RecyclerView.Recycler recycler, int direction) {
+        int positionFirst = getPosition(getChildAt(0));
+        int positionLast = getPosition(getChildAt(getChildCount() - 1));
+
+        //移动过mTotalOffsetX过后的RecyclerViewRECT
+        Rect rectRecyclerView = getRecyclerViewRect();
         //←  dx > 0
         if (direction == DIRECTION_RIGHT_TO_LEFT) {
-            for (int i = 0; i < getChildCount(); i++) {
+            for (int i = getChildCount() - 1; i >= 0; i--) {
                 View child = getChildAt(i);
                 Rect rect = mItems.get(getPosition(child));
-                if (rect.right <= rectRecyclerView.left) {
+                if (!Rect.intersects(rect, rectRecyclerView)) {
+//                    Log.e("xiaojun", "xiaojun111:removeChildPosition=" + getPosition(child));
                     removeAndRecycleView(child, recycler);
                 } else {
                     layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
                 }
             }
-            /*for (int i = positionFirst; i < getChildCount(); i++) {
-                Log.e("xiaojun", "getchildCount=" + getChildCount());
-                Rect rect = mItems.get(i);
-                View child = recycler.getViewForPosition(i);
-                if (rect.right <= rectRecyclerView.left) {
-                    removeAndRecycleView(child, recycler);
-                } else {
-                    layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
-                    requestLayout();
-                }
-            }*/
         } else if (direction == DIRECTION_LEFT_TO_RIGHT) {
-            for (int i = 0; i < getChildCount(); i++) {
+            for (int i = getChildCount() - 1; i >= 0; i--) {
                 View child = getChildAt(i);
                 Rect rect = mItems.get(getPosition(child));
-                if (rect.left >= rectRecyclerView.right) {
+                //如果移动过后的RecyclerView Rect不相交，就移除当前child
+                if (!Rect.intersects(rectRecyclerView, rect)) {
+//                    Log.e("xiaojun", "xiaojun111:removeChildPosition=" + getPosition(child));
                     removeAndRecycleView(child, recycler);
                 } else {
+                    //移动子View
                     layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
                 }
             }
-            /*for (int i = positionLast; i >= 0; i--) {
-                Rect rect = mItems.get(i);
-                View child = recycler.getViewForPosition(i);
-                if (rect.left >= rectRecyclerView.right) {
-                    removeAndRecycleView(child, recycler);
-                } else {
-                    layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
-                }
-            }*/
         }
+
+        addItem(recycler, direction, positionFirst, positionLast);
     }
 
     private void addItem(RecyclerView.Recycler recycler, int direction, int positionFirst, int positionLast) {
-        Rect rectRecyclerView = getScreenRectWithTotalOffsetX();
+//        detachAndScrapAttachedViews(recycler);
+        //移动过后的RecyclerViewRect
+        Rect rectRecyclerView = getRecyclerViewRect();
         //←
         if (direction == DIRECTION_RIGHT_TO_LEFT) {
+            //从第一个到最后一个重新布局
+            /*for (int i = positionFirst; i < getItemCount(); i++) {
+                Rect rect = mItems.get(i);
+                if (Rect.intersects(rectRecyclerView,rect)){
+                    View child = recycler.getViewForPosition(i);
+                    measureChild(child, 0, 0);
+                    addView(child);
+                    layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
+                }
+            }*/
             for (int i = positionLast + 1; i < getItemCount(); i++) {
                 Rect rect = mItems.get(i);
                 if (Rect.intersects(rectRecyclerView, rect)) {
+//                    Log.e("xiaojun","xiaojun111:addView:"+i);
                     View child = recycler.getViewForPosition(i);
-                    addView(child);
                     measureChild(child, 0, 0);
+                    addView(child);
                     layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
                 }
             }
         }
         //→
         else if (direction == DIRECTION_LEFT_TO_RIGHT) {
+            /*for (int i = positionLast; i >= 0 ; i--) {
+                Rect rect = mItems.get(i);
+                if (Rect.intersects(rectRecyclerView, rect)) {
+                    Log.e("xiaojun","xiaojun111:addView:"+i);
+                    View child = recycler.getViewForPosition(i);
+                    measureChild(child, 0, 0);
+                    addView(child, 0);
+                    layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
+                }
+            }*/
+
             for (int i = positionFirst - 1; i >= 0; i--) {
                 Rect rect = mItems.get(i);
                 if (Rect.intersects(rectRecyclerView, rect)) {
+//                    Log.e("xiaojun","xiaojun111:addView:"+i);
                     View child = recycler.getViewForPosition(i);
-                    addView(child, 0);
                     measureChild(child, 0, 0);
+                    addView(child, 0);
                     layoutDecorated(child, rect.left - mTotalOffsetX, rect.top, rect.right - mTotalOffsetX, rect.bottom);
                 }
             }
         }
+
+        //设置当前的最靠近中间线的item
+        getCurrentSelectedItemPosition();
     }
+
+    private int getCurrentSelectedItemPosition(){
+        int positionCenter = 0;
+        //冒泡排序找到最小值
+        Rect rectRecyclerView = getRecyclerViewRect();
+        //中线的x轴坐标
+        int xCenter = (rectRecyclerView.right-rectRecyclerView.left)/2+rectRecyclerView.left;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            Rect rect = mItems.get(getPosition(child));
+            if (rect.left<=xCenter&&rect.right>xCenter){
+                getChildAt(i).setBackgroundColor(Color.RED);
+                positionCenter = i;
+            }else{
+                getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+            }
+        }
+        int positionCenterReal = getPosition(getChildAt(positionCenter));
+        mPositionSelectedItem = positionCenterReal;
+        return positionCenterReal;
+    }
+
+    /*private int getSelectedPosition(){
+        if (getChildCount() == 0)
+            return -1;
+        //找出离中线最近的item
+        for (int i = 0; i < getChildCount(); i++) {
+
+        }
+    }*/
 
 }
